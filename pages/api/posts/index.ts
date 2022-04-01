87213fa27e0c0448423135b3d843effcf7b11844
nohsangwoo <nsgr12@naver.com>
2022-04-01 12:29:30 +0900

    form and handlers

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import withHandler, { ResponseType } from '@libs/server/withHandler'
+import client from '@libs/server/client'
+import { withApiSession } from '@libs/server/withSession'
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    body: { question },
+    session: { user },
+  } = req
+
+  const post = await client.post.create({
+    data: {
+      question,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  })
+
+  return res.json({
+    ok: true,
+    post,
+  })
+}
+
+export default withApiSession(withHandler({ methods: ['POST'], handler }))
