69a7d0ca288f73ca7fa11684429c56f95c85649c
nohsangwoo <nsgr12@naver.com>
2022-04-11 16:53:31 +0900

    work in middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,4 +1,5 @@
 import type { NextRequest, NextFetchEvent } from 'next/server'
+import { NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   console.log('it works! global middleware')
@@ -6,4 +7,10 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.ua?.isBot) {
     return new Response("Plz don't be a bot, Be human", { status: 403 })
   }
+  console.log(req.url)
+  if (!req.url.includes('/api'))
+    if (!req.url.includes('/enter') && !req.cookies.carrotsession)
+      return NextResponse.redirect('/enter')
+
+  console.log('geo: ', req.geo)
 }
