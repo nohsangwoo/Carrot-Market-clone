305f43343ae371fa59cb91cb6a92cdda440c2200
nohsangwoo <nsgr12@naver.com>
2022-06-25 19:00:09 +0900

    fix bug

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -13,18 +13,15 @@ interface ProfileResponse {
 
 export default function useUser() {
   console.log('process.env.NODE_ENV: ')
+
   const { data, error } = useSWR<ProfileResponse>(
-    process.env.NODE_ENV === 'development'
-      ? process.env.APSOLUTE_URL
-      : `${'https://' + process.env.VERCEL_URL}`,
+    `http://localhost:3000/api/users/me`,
   )
   const router = useRouter()
-  console.log(
-    'get apsolute url: ',
-    process.env.NODE_ENV === 'development'
-      ? process.env.APSOLUTE_URL
-      : `${'https://' + process.env.VERCEL_URL}`,
-  )
+  console.log(process.env.NODE_ENV === 'development')
+  console.log(process.env.NEXT_PUBLIC_APSOLUTE_URL_DEV)
+  console.log(process.env.NEXT_PUBLIC_APSOLUTE_URL_PROD)
+
   useEffect(() => {
     if (data && !data.ok) {
       router.replace('/enter')
